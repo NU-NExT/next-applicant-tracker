59a1c8fe4af2c784eabd45d95b8811a80d43005d
Marcus Yi <marcusyicollege@gmail.com>
2026-03-20 15:19:45 -0400

    feat: add ecr

---
+++ infrastructure/terraform/ecr.tf
@@ -0,0 +1,29 @@
+resource "aws_ecr_repository" "frontend" {
+  name                 = "${local.name_prefix}-frontend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_ecr_repository" "backend" {
+  name                 = "${local.name_prefix}-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = local.common_tags
+}
+
+output "ecr_frontend_url" {
+  value = aws_ecr_repository.frontend.repository_url
+}
+
+output "ecr_backend_url" {
+  value = aws_ecr_repository.backend.repository_url
+}
